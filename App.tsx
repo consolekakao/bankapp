acad9949acabf9c92aa6e39dd2508867b90c04fd
consolekakao <console@kakao.com>
2022-11-13 15:20:23 +0900

    feat:[Header] add toLocaleString

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@
  * @format
  */
 
-import React, {useState, type PropsWithChildren} from 'react';
+import React, {useEffect, useState, type PropsWithChildren} from 'react';
 import {
   Image,
   SafeAreaView,
@@ -21,6 +21,7 @@ import {
   View,
 } from 'react-native';
 import styled, {css} from 'styled-components';
+import {memberArr, transactionList} from './data';
 const StyledHeader = styled.View`
   width: 100%;
   height: 260px;
@@ -53,82 +54,22 @@ const StyleMemberButton = styled.View`
   margin-left: ${props => (props.isFirst ? 0 : -10)};
 `;
 
-const memberArr = [
-  {name: '윤지환', url: 'https://avatars.githubusercontent.com/u/60729088?v=4'},
-  {
-    name: '김현수',
-    url: 'https://avatars.githubusercontent.com/u/115357815?v=4',
-  },
-  {name: '유소현', url: 'https://avatars.githubusercontent.com/u/73421899?v=4'},
-  {name: '이근영', url: 'https://avatars.githubusercontent.com/u/79135142?v=4'},
-  {name: '유아영', url: 'https://avatars.githubusercontent.com/u/41851801?v=4'},
-];
-
-const transactionList = [
-  {
-    type: '입금',
-    name: '윤지환',
-    value: 5000,
-    date: '10.03',
-    detail: '고정회비납부',
-  },
-  {
-    type: '입금',
-    name: '윤지환',
-    value: 5000,
-    date: '10.16',
-    detail: '고정회비납부',
-  },
-  {
-    type: '입금',
-    name: '김현수',
-    value: 5000,
-    date: '10.16',
-    detail: '고정회비납부',
-  },
-  {
-    type: '입금',
-    name: '유아영',
-    value: 5000,
-    date: '10.16',
-    detail: '고정회비납부',
-  },
-  {
-    type: '입금',
-    name: '유소현',
-    value: 5000,
-    date: '10.18',
-    detail: '고정회비납부',
-  },
-  {
-    type: '입금',
-    name: '이근영',
-    value: 5000,
-    date: '10.31',
-    detail: '고정회비납부',
-  },
-  {
-    type: '출금',
-    name: '스터디룸(강남)',
-    value: -15400,
-    date: '11.02',
-    detail: '강남역 스터디윙 12시~14시 예약으로 사용',
-  },
-  {
-    type: '입금',
-    name: '윤지환',
-    value: 5000,
-    date: '11.10',
-    detail: '어쩌구저쩌구',
-  },
-];
 const App = () => {
   const [selectedMember, setSelectedMember] = useState<string>('all');
+
   return (
     <>
       <SafeAreaView style={{backgroundColor: 'hotpink'}}>
         <StyledHeader>
-          <StyledTotalMoney>510,490원</StyledTotalMoney>
+          <StyledTotalMoney>
+            {transactionList
+              .reduce((prev: any, curr: any) => {
+                console.log('currrr:', curr.value);
+                return (prev += curr.value);
+              }, 0)
+              .toLocaleString('ko-KR')}
+            원
+          </StyledTotalMoney>
 
           <StyledMembers>
             <TouchableOpacity onPress={() => setSelectedMember('all')}>
---
+++ data.ts
@@ -0,0 +1,69 @@
+export const transactionList = [
+  {
+    type: '입금',
+    name: '윤지환',
+    value: 5000,
+    date: '10.03',
+    detail: '고정회비납부',
+  },
+  {
+    type: '입금',
+    name: '윤지환',
+    value: 5000,
+    date: '10.16',
+    detail: '고정회비납부',
+  },
+  {
+    type: '입금',
+    name: '김현수',
+    value: 5000,
+    date: '10.16',
+    detail: '고정회비납부',
+  },
+  {
+    type: '입금',
+    name: '유아영',
+    value: 5000,
+    date: '10.16',
+    detail: '고정회비납부',
+  },
+  {
+    type: '입금',
+    name: '유소현',
+    value: 5000,
+    date: '10.18',
+    detail: '고정회비납부',
+  },
+  {
+    type: '입금',
+    name: '이근영',
+    value: 5000,
+    date: '10.31',
+    detail: '고정회비납부',
+  },
+  {
+    type: '출금',
+    name: '스터디룸(강남)',
+    value: -15400,
+    date: '11.02',
+    detail: '강남역 스터디윙 12시~14시 예약으로 사용',
+  },
+  {
+    type: '입금',
+    name: '윤지환',
+    value: 5000,
+    date: '11.10',
+    detail: '어쩌구저쩌구',
+  },
+];
+
+export const memberArr = [
+  {name: '윤지환', url: 'https://avatars.githubusercontent.com/u/60729088?v=4'},
+  {
+    name: '김현수',
+    url: 'https://avatars.githubusercontent.com/u/115357815?v=4',
+  },
+  {name: '유소현', url: 'https://avatars.githubusercontent.com/u/73421899?v=4'},
+  {name: '이근영', url: 'https://avatars.githubusercontent.com/u/79135142?v=4'},
+  {name: '유아영', url: 'https://avatars.githubusercontent.com/u/41851801?v=4'},
+];
